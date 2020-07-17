e495925442cdbc4e1da59c96541ebc7e85728fdb
GermainPereira <pereira.germain@outlook.com>
2020-07-17 15:19:55 -0300

    Renamed migration to user. Added create user migration.

--- src/database/migrations/1594979494193-CreateUsers.ts
+++ src/database/migrations/1594979494193-CreateUsers.ts
@@ -1,7 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateServiceProviders1594979494193
-    implements MigrationInterface {
+export default class CreateUsers1594979494193 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
