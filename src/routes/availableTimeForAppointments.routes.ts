935e9da46fd6027c5d35794f6a093ec421c80a57
GermainPereira <pereira.germain@outlook.com>
2020-07-24 15:13:06 -0300

    Added cors

--- src/routes/availableTimeForAppointments.routes.ts
+++ src/routes/availableTimeForAppointments.routes.ts
@@ -1,8 +1,33 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import CreateAvailableTimeForAppointmentsService from '../services/CreateAvailableTimeForAppointmentsService';
-
+import { getRepository } from 'typeorm';
+import availableTimeForAppointments from '../models/AvailableTimeForAppointments';
 const availableTimeForAppointmentsRouter = Router();
+
+availableTimeForAppointmentsRouter.get(
+    '/:user_id',
+    async (request, response) => {
+        try {
+            const { user_id } = request.params;
+            console.log(request.body);
+
+            const availableTimeForAppointmentsRepository = getRepository(
+                availableTimeForAppointments,
+            );
+
+            const availableTimes = await availableTimeForAppointmentsRepository.find(
+                { where: { from_user_id: user_id } },
+            );
+            console.log(availableTimes);
+
+            return response.status(200).json(availableTimes);
+        } catch (err) {
+            console.log(err);
+            return response.status(400).json({ error: err.message });
+        }
+    },
+);
 
 availableTimeForAppointmentsRouter.post(
     '/create/user/:user_id',
--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,13 @@ import express from 'express';
 import errorCatcher from './middlewares/errorCatcher';
 import routes from './routes';
 
+import cors from 'cors';
+
 import 'express-async-errors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
